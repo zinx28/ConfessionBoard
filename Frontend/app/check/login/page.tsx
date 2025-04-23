bbc8c9c1edf475f9197aad4ae465066d8d18db2d
zinx <63958372+zinx28@users.noreply.github.com>
2025-04-23 18:24:54 +0100

    Update page.tsx

--- Frontend/app/check/login/page.tsx
+++ Frontend/app/check/login/page.tsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
         },
         body: JSON.stringify({
            code: disccode,
-           needAccount: true
+           needAccount: boardId ? false : true
         }),
         credentials: 'include'
       });
